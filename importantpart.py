5544cb342247b33fd882f9c2bfc0871a633b4aee
Af4hope <afra.imsupexbd@gmail.com>
2026-02-28 23:16:33 +0600

    Add files via upload

---
+++ importantpart.py
@@ -0,0 +1,13 @@
+# For loop
+for i in range(5):# range(5) generates 0,1,2,3,4
+    print("The number is",i)
+# While loop
+count = 0
+while count < 5:
+    print(count,end=" ")
+    count += 1
+print() # for new line
+def add(a,b):
+    result = a + b
+    return result
+print(add(3,4))
